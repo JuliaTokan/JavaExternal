bb835a66c710c9ce485de740a6b488c9c65ef6b0
Julia Tokan <tokan.yulia@gmail.com>
2020-03-20 14:50:41 +0200

    added demo

--- JDBCMovies/src/org/java/jdbc/DemoMoviesApp.java
+++ JDBCMovies/src/org/java/jdbc/DemoMoviesApp.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
-public class Main {
+public class DemoMoviesApp {
 
     public static void main(String[] args) throws SQLException {
         FilmDAO filmDAO = DaoFactory.createFilmDAO();
